1853d3fe1a28e55699972ed7ec11c05fe332dfd6
Yurii Rabeshko <george.rabeshko@gmail.com>
2017-07-18 17:04:09 +0300

    Configure the workflow

---
+++ dist/vue-formly-buefy.js
@@ -0,0 +1,7 @@
+/**
+ * vue-formly-buefy v1.0.0
+ * https://github.com/yarbshk/vue-formly-buefy.git
+ * Released under the MIT License.
+ */
+
+(function webpackUniversalModuleDefinition(root,factory){if(typeof exports==="object"&&typeof module==="object")module.exports=factory();else if(typeof define==="function"&&define.amd)define([],factory);else if(typeof exports==="object")exports["VueFormlyBuefy"]=factory();else root["VueFormlyBuefy"]=factory()})(this,function(){return function(modules){var installedModules={};function __webpack_require__(moduleId){if(installedModules[moduleId]){return installedModules[moduleId].exports}var module=installedModules[moduleId]={i:moduleId,l:false,exports:{}};modules[moduleId].call(module.exports,module,module.exports,__webpack_require__);module.l=true;return module.exports}__webpack_require__.m=modules;__webpack_require__.c=installedModules;__webpack_require__.d=function(exports,name,getter){if(!__webpack_require__.o(exports,name)){Object.defineProperty(exports,name,{configurable:false,enumerable:true,get:getter})}};__webpack_require__.n=function(module){var getter=module&&module.__esModule?function getDefault(){return module["default"]}:function getModuleExports(){return module};__webpack_require__.d(getter,"a",getter);return getter};__webpack_require__.o=function(object,property){return Object.prototype.hasOwnProperty.call(object,property)};__webpack_require__.p="";return __webpack_require__(__webpack_require__.s=0)}([function(module,exports,__webpack_require__){"use strict";console.log(1)}])});
---
+++ webpack.config.js
@@ -0,0 +1,100 @@
+const webpack = require("webpack")
+const path = require("path")
+const version = require("./package.json").version
+const banner = "/**\n" +
+             " * vue-formly-buefy v" + version + "\n" +
+             " * https://github.com/yarbshk/vue-formly-buefy.git\n" +
+             " * Released under the MIT License.\n" +
+             " */\n"
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+
+const environment = process.env.NODE_ENV || "development"
+const debug = environment === "development"
+
+const rules = [
+    {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"]
+    },
+    {
+        test: /\.vue$/,
+        loader: "vue-loader",
+        options: {
+            extractCSS: !debug,
+            loaders: {
+                "scss": "vue-style-loader!css-loader!sass-loader",
+                "sass": "vue-style-loader!css-loader!sass-loader?indentedSyntax"
+            }
+        }
+    },
+    {
+        test: /\.(sass|scss|css)$/,
+        use: ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: ["css-loader", "sass-loader"]
+        })
+    }
+]
+
+module.exports = [
+    {
+        entry: path.resolve(__dirname, "src/index"),
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "vue-formly-buefy.js",
+            library: "VueFormlyBuefy",
+            libraryTarget: "umd"
+        },
+        module: {
+            rules: rules
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                "process.env": {
+                    NODE_ENV: JSON.stringify(environment)
+                }
+            }),
+            new webpack.optimize.UglifyJsPlugin({
+                compress: false,
+                mangle: false
+            }),
+            new ExtractTextPlugin({
+                filename: "vue-formly-buefy.css",
+                allChunks: true
+            }),
+            new webpack.BannerPlugin({
+                banner: banner,
+                raw: true
+            })
+        ]
+    },
+    {
+        entry: path.resolve(__dirname, "src/index"),
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "vue-formly-buefy.min.js",
+            library: "VueFormlyBuefy",
+            libraryTarget: "umd"
+        },
+        module: {
+            rules: rules
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                "process.env": {
+                    NODE_ENV: JSON.stringify(environment)
+                }
+            }),
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                }
+            }),
+            new webpack.BannerPlugin({
+                banner: banner,
+                raw: true
+            })
+        ]
+    }
+]
