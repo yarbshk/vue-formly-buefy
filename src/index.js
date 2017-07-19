97e83881716a668b66b05fe9d66d654e81c2f831
Yurii Rabeshko <george.rabeshko@gmail.com>
2017-07-19 17:26:47 +0300

    Export the plugin instance correctly

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import Fields from './fields'
 
-let FormlyBuefy = Object()
-
-FormlyBuefy.install = function (Vue, options) {
-    Fields.keys().forEach(key => Vue.$formly.addType(key, Fields(key)))
+const VueFormlyBuefy = {
+  install (Vue, options) {
+    Fields.keys().forEach(key => {
+      Vue.$formly.addType(key, Fields(key))
+    })
+  }
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
-    window.Vue.use(FormlyBuefy)
+  window.Vue.use(VueFormlyBuefy)
 }
 
-return FormlyBuefy
+export default VueFormlyBuefy
