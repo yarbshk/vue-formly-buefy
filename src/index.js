c8c5e5a9fccdca8da95a11f591c5cc8eee97a9c7
Yurii Rabeshko <george.rabeshko@gmail.com>
2017-07-19 11:37:05 +0300

    Create a plugin connector

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log(1)
+import Fields from './fields'
+
+let FormlyBuefy = Object()
+
+FormlyBuefy.install = function (Vue, options) {
+    Fields.keys().forEach(key => Vue.$formly.addType(key, Fields(key)))
+}
+
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(FormlyBuefy)
+}
+
+return FormlyBuefy
