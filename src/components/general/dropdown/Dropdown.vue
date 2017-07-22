1510ad5ef8d03eed1d9f7c8aad37af8c8d6473b2
Yurii Rabeshko <george.rabeshko@gmail.com>
2017-07-22 17:10:11 +0300

    Add secondary Dropdown component

---
+++ src/components/general/dropdown/Dropdown.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-dropdown v-bind="properties">
+    <vfb-button class="button"
+                slot="trigger"
+                v-bind="button">
+    </vfb-button>
+    <vfb-dropdown-option v-for="(option, index) in options"
+                         v-bind="option"
+                         v-bind:key="index">
+    </vfb-dropdown-option>
+  </b-dropdown>
+</template>
+
+<script>
+  import VfbButton from '../button/Button.vue'
+  import VfbDropdownOption from './DropdownOption.vue'
+  export default {
+    props: {
+      button: Object,
+      options: Array,
+      properties: {
+        type: Object,
+        default: {}
+      }
+    },
+    components: {
+      VfbButton,
+      VfbDropdownOption
+    }
+  }
+</script>
---
+++ src/components/general/dropdown/DropdownOption.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-dropdown-option v-bind="properties">
+    <b-icon v-if="icon" :icon="icon"></b-icon>
+    <span>{{ label }}</span>
+  </b-dropdown-option>
+</template>
+
+<script>
+  export default {
+    props: {
+      label: String,
+      icon: {
+        type: String,
+        default: ''
+      },
+      properties: {
+        type: Object,
+        default: {}
+      }
+    }
+  }
+</script>
