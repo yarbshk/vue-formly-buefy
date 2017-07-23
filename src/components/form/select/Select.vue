4f947df20d5afaa91fbc98f8dd9aebf6e5590c81
Yurii Rabeshko <george.rabeshko@gmail.com>
2017-07-24 01:33:05 +0300

    Add base implementation of a select component

---
+++ src/components/form/select/Select.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-select v-bind="properties"
+            v-model="model[field.key]"
+            @blur="handleBlurEvent"
+            @focus="handleFocusEvent"
+            @change="handleChangeEvent">
+    <!-- TODO: Add options listing -->
+  </b-select>
+</template>
+
+<script>
+  /**
+   * Implements a usual select field (a dropdown list with options)
+   * Look at API section (the link below) for the reference
+   * {@link https://buefy.github.io/#/documentation/select}
+   */
+  import WrappedFormlyFieldMixin from 'src/mixins/wrapped-formly-field.mixin'
+
+  export default {
+    mixins: [WrappedFormlyFieldMixin]
+  }
+</script>
