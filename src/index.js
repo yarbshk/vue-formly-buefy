7f91486cdc3db5ad41cabe3f8cfb7a5b20312a97
Yurii Rabeshko <george.rabeshko@gmail.com>
2017-12-26 07:30:03 +0200

    Update supported Formly types

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { addTypes, wrapComponent } from './utils'
 
 import Autocomplete from './components/form/autocomplete'
 import Datepicker from './components/form/datepicker'
-import Field from './components/form/field'
 import Input from './components/form/input'
 import Checkbox from './components/form/checkbox'
 import Radio from './components/form/radio'
@@ -12,18 +11,25 @@ import Taginput from './components/form/taginput'
 import Timepicker from './components/form/timepicker'
 import Upload from './components/form/upload'
 
+import MultiBlock from './components/wrappers/block'
+import Field from './components/wrappers/field'
+
 const VueFormlyBuefy = {
   install (Vue, options) {
     addTypes(Vue.$formly, {
       'autocomplete': Autocomplete,
       'autocomplete-with-field': wrapComponent(Autocomplete, Field.Default),
       'checkbox': Checkbox.Default,
+      'checkbox-with-block': wrapComponent(Checkbox.Default, MultiBlock),
+      'checkbox-with-field': wrapComponent(Checkbox.Default, Field.MultiField),
       'checkbox-button': wrapComponent(Checkbox.CheckboxButton, Field.MultiField),
       'datepicker': Datepicker,
       'datepicker-with-field': wrapComponent(Datepicker, Field.Default),
       'input': Input,
       'input-with-field': wrapComponent(Input, Field.Default),
       'radio': Radio.Default,
+      'radio-with-block': wrapComponent(Radio.Default, MultiBlock),
+      'radio-with-field': wrapComponent(Radio.Default, Field.MultiField),
       'radio-button': wrapComponent(Radio.RadioButton, Field.MultiField),
       'select': Select,
       'select-with-field': wrapComponent(Select, Field.Default),
