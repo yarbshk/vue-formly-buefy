c4c2bbce51826e81d46f3521a29a84809d037214
Yurii Rabeshko <george.rabeshko@gmail.com>
2017-12-27 02:46:12 +0200

    Rename controls library

---
+++ src/controls.js
@@ -0,0 +1,5 @@
+import Area from './components/controls/area'
+import Button from './components/controls/button'
+import Span from './components/controls/span'
+
+export { Area, Button, Span }
--- src/plain-controls.js
+++
@@ -1,4 +0,0 @@
-import Button from './components/general/button'
-import Span from './components/general/span'
-
-export { Button, Span }
